f5e78845d10ae9744e22d6bf43995c87c201e6bf
FerdosFijer <ffijer@gmail.com>
2026-07-05 16:46:04 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Countries from './components/Counries/Countries'
+
+function App() {
+
+  return (
+    <>
+    <Countries> </Countries>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Counries/Countries.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Countries = () => {
+    return (
+        <div>
+            <h2>I am in the country</h2>
+        </div>
+    );
+};
+
+export default Countries;
