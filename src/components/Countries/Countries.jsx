d8429f3b839c04d68c25272102753f90354ef326
FerdosFijer <ffijer@gmail.com>
2026-07-06 20:38:47 +0600

    Done full project

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -1,22 +1,47 @@
-import React, { use } from 'react';
-import Country from '../Country/Country';
+import React, { use, useState } from "react";
+import Country from "../Country/Country";
+import "./countries.css";
 
-const Countries = ({countriesPromise}) => {
-    const countriesData = use (countriesPromise); //! 1 ta object file e 3 ta file paisi, counties e dukbo const countries diye
-    // console.log(countriesData);
-    const countries = countriesData.countries;  //! dot.countries ta json obj er virote paisi and er por array ache tai map calabo
-    // console.log(countries);
+const Countries = ({ countriesPromise }) => {
+  const countriesData = use(countriesPromise); //! 1 ta object file e 3 ta file paisi, counties e dukbo const countries diye
+  // console.log(countriesData);
+  const countries = countriesData.countries; //! dot.countries ta json obj er virote paisi and er por array ache tai map calabo
+  // console.log(countries);
 
-    return (
-        <div>
-            <h1>Total Country: {countries.length} </h1>
+  //! lift up the state next 5 line kortesi karon: Amra protome akta state decleare korbo
+  //! handaler function banabo and handaler ta map kore props ta  button er file a patabo distructuring kore
+  //! seta params akare receive korbe and params akare receive howa file ta button file e handlaer e add korbo
+  //!  and button er main params ta k abr protomer file a patabo handaler e and ota diye kaj korbo
 
-             {/*//! Map tkne korboo jkn Array asbe [] */}
-            {
-                countries.map(country1 => <Country key={country1.cca3.cca3} country={country1}> </Country>)
-            }
-        </div>
-    );
+  const [visitedCountries, setVisitedCountries] = useState([]);
+  const handleVisitedCountries = (country) => {
+    console.log("handle visited country clicked", country);
+    const newVisitedCountries = [...visitedCountries, country];
+    setVisitedCountries(newVisitedCountries);
+  };
+
+  return (
+    <>
+      <h1>Total Country: {countries.length} </h1>
+      <h2>Total Country Visited : {visitedCountries.length} </h2>
+
+      <ol>
+       { visitedCountries.map (country2  =>  <li key={country2.cca3.cca3}> {country2.name.common}</li>)}
+      </ol>
+
+      <div className="countries">
+        {/*//! Map tkne korboo jkn Array asbe [] */}
+        {countries.map((country1) => (
+          <Country
+            //! country1 map er props and country params hisebe patay
+            key={country1.cca3.cca3}
+            handleVisitedCountries={handleVisitedCountries}
+            country={country1}
+          ></Country>
+        ))}
+      </div>
+    </>
+  );
 };
 
 export default Countries;
--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,16 +1,34 @@
-import React from 'react';
+import React, { useState } from "react";
+import "./country.css";
 
-const Country = ({country}) => {
-    console.log(country);
-    
+const Country = ({ country, handleVisitedCountries }) => {
+  //   console.log(country);
+  //   console.log(handleVisitedCountries);
 
-    return (
-        <div style={{ paddingBottom: '20px' }}>
-            <img src={country.flags.flags.png} alt={country.flags.flags.alt} srcset="" />
-            <h3>Name:{country.name.common} </h3>
-            <p>Population: {country.population.population}</p>
-        </div>
-    );
+  const [visited, setVisited] = useState(false);
+  const handleVisited = () => {
+    // console.log("btn clicked ");
+    // setVisited(true);
+    // setVisited(visited? false: true);
+    setVisited(!visited); //! toggle kore felbe true thakle false kore fele and uporer 3 line same as this line
+    handleVisitedCountries(country);
+  };
+
+  return (
+    //!kivabe conditional add korbo class e & bracket r tamplate string dibo
+    <div className={`country ${visited && "country-visited"}`}>
+      <img
+        src={country.flags.flags.png}
+        alt={country.flags.flags.alt}
+        srcset=""
+      />
+      <h3>Name:{country.name.common} </h3>
+      <p>Population: {country.population.population}</p>
+      <button onClick={handleVisited}>
+        {visited ? "Visited" : "Not Visited"}
+      </button>
+    </div>
+  );
 };
 
 export default Country;
