497e2508fe0da61844872f67d4dc60ee3aa36d0b
FerdosFijer <ffijer@gmail.com>
2026-07-06 11:22:40 +0600

    I have done country details

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -1,16 +1,20 @@
 import React, { use } from 'react';
+import Country from '../Country/Country';
 
 const Countries = ({countriesPromise}) => {
-    const countriesData = use (countriesPromise);
+    const countriesData = use (countriesPromise); //! 1 ta object file e 3 ta file paisi, counties e dukbo const countries diye
     // console.log(countriesData);
-    const countries = countriesData.countries;  //! countriesData.countries ta json er obj er virote paisi
+    const countries = countriesData.countries;  //! dot.countries ta json obj er virote paisi and er por array ache tai map calabo
     // console.log(countries);
 
     return (
         <div>
-            <h1>Hi I am here: {countries.length} </h1>
-            <h1>Hi I am here: {countries.area} </h1>
-            
+            <h1>Total Country: {countries.length} </h1>
+
+             {/*//! Map tkne korboo jkn Array asbe [] */}
+            {
+                countries.map(country1 => <Country key={country1.cca3.cca3} country={country1}> </Country>)
+            }
         </div>
     );
 };
--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 
-const Country = () => {
+const Country = ({country}) => {
+    console.log(country);
+    
+
     return (
-        <div>
-            
+        <div style={{ paddingBottom: '20px' }}>
+            <img src={country.flags.flags.png} alt={country.flags.flags.alt} srcset="" />
+            <h3>Name:{country.name.common} </h3>
+            <p>Population: {country.population.population}</p>
         </div>
     );
 };
